71d1b94bf59fb2ec90c052941e8d4430cd69565d
Emi-legui <emilegui76@gmail.com>
2026-06-03 15:06:59 -0300

    Creando testing

--- test/calculadoratesting/CalculadorTest.java
+++ test/calculadoratesting/CalculadorTest.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
+
 package calculadoratesting;
 
 import org.junit.jupiter.api.AfterEach;
@@ -17,39 +14,19 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class CalculadorTest {
     
-    public CalculadorTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
-
-    /**
-     * Test of sumar method, of class Calculador.
-     */
-    @Test
+  @Test
+  
     public void testSumar() {
-        System.out.println("sumar");
-        double a = 0.0;
-        double b = 0.0;
-        Calculador instance = new Calculador();
-        double expResult = 0.0;
-        double result = instance.sumar(a, b);
-        assertEquals(expResult, result, 0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        System.out.println("Probando el metodo: Sumar");
+        
+        Calculador calcTester = new Calculador();
+        
+        double a = 5.5;
+        double b = 4.5;
+        double esperado = 10.0;
+          
+        double resultadoReal = calcTester.sumar(a, b);
+        
+        assertEquals(esperado, resultadoReal, 0.001); 
     }
-    
 }
