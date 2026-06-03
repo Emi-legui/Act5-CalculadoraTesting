9c60f0cd49fa01a157e7fb2824a4188f7fc4a667
Emi-legui <emilegui76@gmail.com>
2026-06-03 14:57:02 -0300

    creando metodos para las operaciones principales

--- src/calculadoratesting/Calculador.java
+++ src/calculadoratesting/Calculador.java
@@ -3,7 +3,18 @@ package calculadoratesting;
 
 public class Calculador {
     
+    
+    //metodos
     public double sumar(double a, double b) {
         return a + b;
+    }
+    public double restar(double a, double b){
+        return a - b;
+    }
+    public int multiplicar(int a, int b){
+        return a *  b;
+    }
+    public int dividir(int a, int b){
+        return a / b;
     }
 }
