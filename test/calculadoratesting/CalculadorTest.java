ca367e23ad2ed5981e0054b4a0e19db577b9343e
Emi-legui <emilegui76@gmail.com>
2026-06-03 15:16:25 -0300

    agregando test sumar y restar

--- test/calculadoratesting/CalculadorTest.java
+++ test/calculadoratesting/CalculadorTest.java
@@ -15,8 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CalculadorTest {
     
   @Test
-  
-    public void testSumar() {
+  public void testSumar() {
         System.out.println("Probando el metodo: Sumar");
         
         Calculador calcTester = new Calculador();
@@ -28,5 +27,19 @@ public class CalculadorTest {
         double resultadoReal = calcTester.sumar(a, b);
         
         assertEquals(esperado, resultadoReal, 0.001); 
+    }
+  @Test
+    public void testRestar() {
+        System.out.println("Probando el metodo: Restar");
+        
+        Calculador calcTester = new Calculador();
+        
+        double a = 10.0;
+        double b = 3.5;
+        double esperado = 6.5;
+        
+        double resultadoReal = calcTester.restar(a, b);
+        
+        assertEquals(esperado, resultadoReal, 0.001);
     }
 }
